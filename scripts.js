8323696a33675a5b6b9f76fcb2e943ed1f870cf7
Sammy B <sam.bradbury@hotmail.com>
2025-03-24 14:54:08 +1100

    working with json object and using in table

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 // alert("Page loaded");
 console.log("loaded");
 
-fetch("test.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
+// fetch("test.json")
+//   .then(response => response.json())
+//   .then(json => console.log(json));
+
+function getData(url, cb) {
+  fetch(url)
+    .then(response => response.json())
+    .then(result => cb(result));
+}
+
+getData("./test.json", (data) => console.log({ data }))
